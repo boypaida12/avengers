c415db2c06c4613dd6a4649bdc6687945f37fe94
boypaida12 <baddysackey5@gmail.com>
2023-03-18 14:24:35 +0000

    added img and custom css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './unnamed.jpeg';
+
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main className="app">
+      <section className="hero-section">
+        <p style={{textAlign:"center", fontSize:"4rem"}}>
+          Codetrain is Awesome!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <img src={logo} alt='Codetrain students posing for a picture' width={500} />
+        
+      </section>
+    </main>
   );
 }
 
